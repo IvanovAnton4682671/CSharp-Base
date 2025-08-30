c9fb56bbabcf92e8743245f981a4caaf8fa7863f
IvanovAnton4682671 <anton-ivanov-080203@mail.ru>
2025-08-30 14:14:00 +0300

    updated dataTypes

--- ConsoleApp/0_Basics/0_Data_Types.cs
+++ ConsoleApp/0_Basics/0_Data_Types.cs
@@ -2,7 +2,8 @@
 // ЗНАЧИМЫЕ ТИПЫ (Value Types)
 // Что это? Переменные этих типов содержат свои данные напрямую. Когда ты присваиваешь одну value-type переменную другой, копируется значение
 // Где хранятся? Они хранятся в области памяти под названием стек (stack). Работа со стеком очень быстрая
-// К ним относятся: числовые типы, char, bool, struct (позже), enum (позже)
+// Поведение? При присваивании копируются все данные
+// К ним относятся такие типы как: числовые (со знаком, без знака, с плавающей запятой), логический, символьный, кортеж, перечисление, структура
 
 // 1. Целые числа (со знаком)
 sbyte sbVar = 120;        // 1 байт (-128 до 127)
@@ -29,6 +30,53 @@ bool isEnabled = false;   // Ложь
 char symbol = 'a';     // 2 байта (хранит один символ Unicode в одинарных кавычках)
 char newLine = '\n';   // Управляющая последовательность для новой строки
 
+// 6. Кортеж
+// Представляет собой неизменяемую последовательность
+// Все функции возвращают значения в виде кортежей, но об этом позже
+(int, string) person = (20, "Alice");                                 // Неименованный кортеж
+Console.WriteLine($"person item1={person.Item1}, person item 2={person.Item2}");
+(int age, string name) namedPerson = (20, "Alice");                   // Именованный кортеж
+Console.WriteLine($"namedPerson age={namedPerson.age}, namedPerson name={namedPerson.name}");
+
+// 7. Перечисление
+// Позволяет хранить некоторую последовательность данных
+enum Weekday { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };   // Без заданного базового типа
+Weekday today = Weekday.Monday;
+Console.WriteLine($"today={today}");
+Console.WriteLine($"int today={(int)today}");
+enum ErrorCode : ushort { None = 0, NotFound = 404, InternalError = 500 };         // С заданным базовым типом
+ErrorCode notFound = ErrorCode.NotFound;
+Console.WriteLine($"notFound={notFound}");
+Console.WriteLine($"ushort notFound={(ushort)notFound}");
+
+// 8. Структура
+// Очень похода на класс, но является значимым типом
+// Рекомендуется использовать когда нужно часто создавать и удалять небольшие переменные (класс будет работать дольше)
+public struct Point
+{
+    // Поля структуры
+    public int X;
+    public int Y;
+
+    // Конструктор структуры
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // Метод структуры
+    public void Print()
+    {
+        Console.WriteLine($"X={X}, Y={Y}");
+    }
+}
+Point p1 =  new Point(1, 2);   //создание экземпляра структуры с аргументами для конструктора
+Point p2 = p1;
+p2.X = 10;
+p1.Print();
+p2.Print();
+
 // Поведение
 // При присваивании значение копируется
 int a = 10;
@@ -42,32 +90,102 @@ Console.WriteLine($"a={a}, b={b}");
 // ССЫЛОЧНЫЕ ТИПЫ (Reference Types)
 // Что это? Переменные этих типов не содержат данные напрямую, а содержат ссылку (адрес в памяти) на область, где эти данные хранятся
 // Где хранятся? Данные (объекты) хранятся в области памяти под названием куча (heap). Сама переменная (ссылка) хранится в стеке
-// К ним относятся: string, массивы (позже), class (позже), interface (позже), delegate (позже)
+// Поведение? При присваивании копируется только ссылка на исходный объект
+// К ним относятся такие типы как: объект, строка, динамический, массив, класс, интерфейс, делегат, 
 
-// 1. Строки
-string name = "Alice";
+// 1. Объект
+// Для объекта существуют такие понятия как Упаковка (Boxing) и Распаковка (Unboxing)
+// Упаковка - процесс преобразования значимого типа в ссылочный object
+// Распаковка - процесс преобразования ссылочного object обратно в значимый тип
+object obj1 = "I am a string";   // Строка просто присваивается переменной object
+object obj2 = 10;                // Упаковка
+int obj3 = (int)obj2;            //Распаковка
+Console.WriteLine($"obj1 type={obj1.GetType()}, obj2 type={obj2.GetType()}, obj3 type={obj3.GetType()}");
 
-// 2. Массивы
+// 2. Строки
+// У строк особое поведение: стандартное поведение когда несколько переменных ссылаются на одно значение
+// Но при изменении значения одной из переменных создаётся новая строка
+string alice = "Alice";
+string alice2 = alice;
+string alice3 = alice;
+alice3 = "Alice3";
+Console.WriteLine($"alice={alice}, alice2={alice2}, alice3={alice3}");
+
+// 3. Динамический тип
+// На этапе компиляции проверка типов пропускается, выполняется только во время выполнения. Не рекомендуется использовать
+dynamic dynVar = "string";
+Console.WriteLine($"dynVar type = {dynVar.GetType()}, dynVar={dynVar}");
+dynVar = 10;
+Console.WriteLine($"dynVar type={dynVar.GetType()}, dynVar={dynVar}");
+
+// 4. Массивы
 int[] intArray = [1, 2, 3, 4, 5];
 string[] strArray = ["Alice", "Bob", "Clare"];
 
-// Поведение
-// Строки ведут себя по-особенному: это ссылочный тип, но при изменении значения по ссылке создаётся новое значение
-string name1 = "Alice";
-string name2 = name1;
-Console.WriteLine($"name1={name1}, name2={name2}");
-name2 = "Bob";
-Console.WriteLine($"name1={name1}, name2={name2}");
-// Другие ссылочные типы ведут себя одинаково: при изменении значения по ссылке меняются все переменные, которые ссылались на это значение
-int[] integerArray1 = [1, 2, 3, 4, 5];
-int[]  integerArray2 = integerArray1;
-Console.WriteLine($"integerArray1={string.Join(", ", integerArray1)}, integerArray2={string.Join(", ", integerArray2)}");
-integerArray2[0] = 0;
-Console.WriteLine($"integerArray1={string.Join(", ", integerArray1)}, integerArray2={string.Join(", ", integerArray2)}");
+// 5. Класс
+// Как структура, но ссылочный тип. Рекомендуется применять везде
+public class Point2
+{
+    // Поля класса
+    public int X;
+    public int Y;
+
+    // Конструктор класса
+    public Point2(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // Метод класса
+    public void Print()
+    {
+        Console.WriteLine($"X={X}, Y={Y}");
+    }
+}
+Point2 p1 = new Point2(1, 2);
+Point2 p2 = p1;
+p2.Y = 10;
+p1.Print();
+p2.Print();
+
+// 6. Интерфейс
+public interface IPoint2
+{
+    void Print();
+}
+
+// 7. Делегат
+// Делегат - это переменная, которая является функцией
+// Сигнатура - возвращаемый тип + принимаемые аргументы
+public delegate void PrintHandler(string message);        // Объявление делегата
+public void PrintMessage(string message)                  // Какая-то функция с сигнатурой как у делегата
+{
+    Console.WriteLine(message);
+}
+PrintHandler delegat1 = new PrintHandler(PrintMessage);   // Создание делегата
+delegat1("Hello");                                // Вызов делегата
 
 
 
-// КЛЮЧЕВОЕ СЛОВО null
+// ПСЕВДОНИМЫ ТИПОВ
+// Псевдоним - это другое название, которое упрощает использование стандартного
+// Например: string - псевдоним для System.String, int - для System.Int32
+string str1 = "Hello";
+System.String str2 = "World";
+int int1 = 120;
+System.Int32 int2 = 120;
+
+
+
+// КЛЮЧЕВЫЕ СЛОВА
+// 1. var
+// var позволяет компилятору самому определить тип на этапе компиляции
+int int3 = 150;
+var int4 = 200;         // Компилятор сам определит тип
+var str3 = "string4";   // Компилятор понимает по правой части что это строка
+
+// 2. null
 // null - значение по умолчанию для ссылочных типов
 string s1 = null;
 Console.WriteLine($"s1={s1}");
